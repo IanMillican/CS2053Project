f2c58026b7af2985f0234831ce29f8208fbe7c23
Ian Millican <149719874+IanMillican@users.noreply.github.com>
2025-04-18 23:19:30 -0300

    Updated script and fixed screen size issue on another cut scene

--- cs2053-thegame/2DMainMenu/intro_cut_scene.gd
+++ cs2053-thegame/2DMainMenu/intro_cut_scene.gd
@@ -5,14 +5,14 @@ var currentLine = 0
 var leftAlign = false
 
 var lines = [
-	"Okay okay that first game wasn't so hard!",
-	"You just wait, they get harder",
-	"What you mean they get harder? \nAren't we just mkaing fun easy games?",
-	"If that's what you expect you're in a long term.",
-	"Woah woah woah what just happened??",
-	"What do you mean what happened?",
-	"WHY AM I PAC-MAN NOW???",
-	"You've literally always been Pac-Man, \nI don't know what you're so worked up about."
+	"Where am I?!",
+	"You fell asleep while studying, so naturally \nyour subconcious has chosen to punish you",
+	"Why is my subconcious my CS2053 prof",
+	"WhY iS mY sUbCoNcIoUs mY cS2053 pRof???",
+	"...",
+	"...",
+	"real mature",
+	"You have to complete a series of levels \nbased on your assignments to get out of here."
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -40,6 +40,6 @@ func introButton() -> void:
 			label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 		leftAlign = !leftAlign
 		label.text = lines[currentLine]
-	if currentLine == 4:
+	if currentLine == 7:
 		$player.visible = true
 		$AnimatedSprite2D.visible = false
